be4eda844711c4426e3cbe4a278ea0313b3362c4
Panos <pkokki@gmail.com>
2020-03-08 01:14:28 +0200

    added spark.start application property

--- src/main/java/com/panos/sportmonitor/spark/ApplicationStartup.java
+++ src/main/java/com/panos/sportmonitor/spark/ApplicationStartup.java
@@ -1,16 +1,23 @@
 package com.panos.sportmonitor.spark;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ApplicationStartup implements ApplicationListener<ContextRefreshedEvent> {
+    @Value("${spark.start}")
+    private boolean sparkStart;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        ApplicationContext ac = event.getApplicationContext();
-        SparkKafkaStreamExecutor sparkKafkaStreamExecutor= ac.getBean(SparkKafkaStreamExecutor.class);
-        Thread thread = new Thread(sparkKafkaStreamExecutor);
-        thread.start();
+        if (sparkStart) {
+            ApplicationContext ac = event.getApplicationContext();
+            SparkKafkaStreamExecutor sparkKafkaStreamExecutor = ac.getBean(SparkKafkaStreamExecutor.class);
+            Thread thread = new Thread(sparkKafkaStreamExecutor);
+            thread.start();
+        }
     }
 }
