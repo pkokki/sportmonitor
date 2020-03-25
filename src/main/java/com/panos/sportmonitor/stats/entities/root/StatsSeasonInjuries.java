baf36f636e6ad691a408526803ab6b4b01b240ab
Panos <pkokki@gmail.com>
2020-03-25 22:35:55 +0200

    completed StatsSeasonInjuries

--- src/main/java/com/panos/sportmonitor/stats/entities/root/StatsSeasonInjuries.java
+++ src/main/java/com/panos/sportmonitor/stats/entities/root/StatsSeasonInjuries.java
@@ -3,13 +3,8 @@ package com.panos.sportmonitor.stats.entities.root;
 import com.panos.sportmonitor.stats.BaseEntity;
 import com.panos.sportmonitor.stats.BaseRootEntity;
 import com.panos.sportmonitor.stats.BaseRootEntityType;
-import com.panos.sportmonitor.stats.EntityIdList;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class StatsSeasonInjuries extends BaseRootEntity {
-    private EntityIdList playerStatuses = new EntityIdList();
 
     public StatsSeasonInjuries(long timeStamp) {
         super(BaseRootEntityType.StatsSeasonInjuries, timeStamp);
@@ -17,19 +12,16 @@ public class StatsSeasonInjuries extends BaseRootEntity {
 
     @Override
     protected boolean handleChildEntity(String entityName, BaseEntity childEntity) {
-        switch (entityName) {
-            case "[]": this.playerStatuses.add(childEntity.getId()); break;
-            default: return super.handleChildEntity(entityName, childEntity);
+        if ("[]".equals(entityName)) {
+            return true;
+        } else {
+            return super.handleChildEntity(entityName, childEntity);
         }
-        return true;
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("StatsSeasonInjuries{");
-        sb.append("name=").append(getName());
-        sb.append(", playerStatuses=").append(playerStatuses);
-        sb.append('}');
-        return sb.toString();
+        return "StatsSeasonInjuries{" + "name=" + getName() +
+                '}';
     }
 }
