031be7a1865e8b02d8f9aba86f25e9dcaa92c2e5
Panos <pkokki@gmail.com>
2020-03-27 19:16:21 +0200

    remove associations package

--- src/main/java/com/panos/sportmonitor/stats/entities/associations/SimpleEntityAssociation.java
+++
@@ -1,9 +0,0 @@
-package com.panos.sportmonitor.stats.entities.associations;
-
-import com.panos.sportmonitor.stats.BaseEntity;
-
-public class SimpleEntityAssociation extends BaseEntityAssociation {
-    public SimpleEntityAssociation(BaseEntity master, Class<? extends BaseEntity> childEntityClass) {
-        super(master, childEntityClass);
-    }
-}
